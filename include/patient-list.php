d88f03bf789caf5d74697e2fe54cd975eab0ecf2
TuNQuang <71865157+TuNQuang@users.noreply.github.com>
2021-08-30 10:05:00 +0900

    Update patient-list.php

--- include/patient-list.php
+++ include/patient-list.php
@@ -25,28 +25,47 @@
 	
 	require_once('../mysqli_connect.php');
 	
-	$query = "SELECT * FROM `benhnhan`  inner join `lichkham` on benhnhan.bnid = lichkham.bnid inner join services on lichkham.services = services.sid WHERE lichkham.pending = '0' order by lichkham.ngaykham DESC";
-	$q = mysqli_stmt_init($dbcon);
-	mysqli_stmt_prepare($q, $query);
-	mysqli_stmt_execute($q);
+	$query2 = 'select DISTINCT bnid from lichkham';
+	$q2 = mysqli_stmt_init($dbcon);
+	mysqli_stmt_prepare($q2, $query2);
+	mysqli_stmt_execute($q2);
 	
-	$result = mysqli_stmt_get_result($q);
-	while ($row = mysqli_fetch_assoc($result))
+	$result2 = mysqli_stmt_get_result($q2);
+	while($row2 = mysqli_fetch_assoc($result2))
 	{
-		echo '<div class = "pending-column bottom-border dotted-border">';
-			echo '<div class = "pending-item  name-column">
-					<a href = "patient-info.php?bnid='. $row['bnid']. '">';
-			echo		$row['bnname'];
-			echo	'</a>
-				</div>';
-			
-			echo '<div class = "pending-item  service-column">';
-			echo		$row['sname'];
-			echo '</div>';
-			
-			echo '<div class = "pending-item  name-column">';
-			echo		$row['ngaykham'];
-			echo	'</div>';
-			echo '</div>';
+		$query = "SELECT * FROM `benhnhan`  
+				inner join `lichkham` on benhnhan.bnid = lichkham.bnid 
+				inner join services on lichkham.services = services.sid 
+				WHERE lichkham.pending = '0' 
+					and benhnhan.bnid = ? 
+					and lichkham.ngaykham = (select max(ngaykham) from lichkham where bnid = ?)";
+		$q = mysqli_stmt_init($dbcon);
+		mysqli_stmt_prepare($q, $query);
+		mysqli_stmt_bind_param($q, "ss", $row2['bnid'], $row2['bnid']);
+		mysqli_stmt_execute($q);
+		
+		$result = mysqli_stmt_get_result($q);
+		while ($row = mysqli_fetch_assoc($result))
+		{
+			echo '<div class = "pending-column bottom-border dotted-border">';
+				echo '<div class = "pending-item  name-column">
+						<a href = "patient-info.php?bnid='. $row['bnid']. '">';
+				echo		$row['bnname'];
+				echo	'</a>
+					</div>';
+				
+				echo '<div class = "pending-item  service-column">';
+				echo		$row['sname'];
+				echo '</div>';
+				
+				echo '<div class = "pending-item  name-column">';
+				echo		$row['ngaykham'];
+				echo	'</div>';
+				echo '</div>';
+		}
 	}
+	
+	
+	
+	
 ?>
