05ac0c3a1b8784b7d225e64e626021a602186cc5
TuNQuang <71865157+TuNQuang@users.noreply.github.com>
2021-08-30 09:58:01 +0900

    Update patient-list.php

--- include/patient-list.php
+++ include/patient-list.php
@@ -25,27 +25,28 @@
 	
 	require_once('../mysqli_connect.php');
 	
-	$query = "SELECT * FROM `benhnhan` inner join `lichkham` on benhnhan.bnid = lichkham.bnid inner join services on lichkham.services = services.sid order by lichkham.ngaykham DESC";
+	$query = "SELECT * FROM `benhnhan`  inner join `lichkham` on benhnhan.bnid = lichkham.bnid inner join services on lichkham.services = services.sid WHERE lichkham.pending = '0' order by lichkham.ngaykham DESC";
 	$q = mysqli_stmt_init($dbcon);
 	mysqli_stmt_prepare($q, $query);
 	mysqli_stmt_execute($q);
 	
 	$result = mysqli_stmt_get_result($q);
-	$row = mysqli_fetch_assoc($result);
-	
-	echo '<div class = "pending-column bottom-border dotted-border">';
-		echo '<div class = "pending-item  name-column">
-				<a href = "patient-info.php?bnid='. $row['bnid']. '">';
-		echo		$row['bnname'];
-		echo	'</a>
-			</div>';
-		
-		echo '<div class = "pending-item  service-column">';
-		echo		$row['sname'];
-		echo '</div>';
-		
-		echo '<div class = "pending-item  name-column">';
-		echo		$row['ngaykham'];
-		echo	'</div>';
-		echo '</div>';
+	while ($row = mysqli_fetch_assoc($result))
+	{
+		echo '<div class = "pending-column bottom-border dotted-border">';
+			echo '<div class = "pending-item  name-column">
+					<a href = "patient-info.php?bnid='. $row['bnid']. '">';
+			echo		$row['bnname'];
+			echo	'</a>
+				</div>';
+			
+			echo '<div class = "pending-item  service-column">';
+			echo		$row['sname'];
+			echo '</div>';
+			
+			echo '<div class = "pending-item  name-column">';
+			echo		$row['ngaykham'];
+			echo	'</div>';
+			echo '</div>';
+	}
 ?>
